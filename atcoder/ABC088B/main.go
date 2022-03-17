037c72080f8987d1d0895416905d320eb05817f0
MH4GF <h.miyagi.cnw@gmail.com>
2022-03-17 22:57:18 +0900

    2022/03/17

---
+++ atcoder/ABC088B/main.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+)
+
+// Returns next token from input.  It must be initialized by SetInput()
+// before the first call.
+var nextToken func() ([]byte, error)
+
+// Holds a buffer for output.  It must be initialized by SetOutput().
+// All IO fucntions (read*() and [e]print*()) should write to OutputWriter
+// instead of this.
+var OutputBuffer *bufio.Writer
+
+// Holds an io.Writer.  It must be initialized by SetOutput()
+var OutputWriter io.Writer
+
+func readb() []byte {
+	b, err := nextToken()
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+// Returns true if c is a white space
+func IsSpace(c byte) bool {
+	switch c {
+	case '\t', '\n', '\v', '\f', '\r', ' ':
+		return true
+	}
+	return false
+}
+
+// Setup nextToken with input buffer.
+func SetInput(r io.Reader) {
+	buf := new(bytes.Buffer)
+	var b []byte
+
+	var i int
+	rest := func() ([]byte, error) {
+		for i < len(b) && IsSpace(b[i]) {
+			i++
+		}
+		j := i
+		for i < len(b) && !IsSpace(b[i]) {
+			i++
+		}
+		if i == j {
+			return nil, io.ErrUnexpectedEOF
+		}
+		return b[j:i], nil
+	}
+	initial := func() ([]byte, error) {
+		io.Copy(buf, r)
+		b = buf.Bytes()
+		nextToken = rest
+		return rest()
+	}
+	nextToken = initial
+}
+
+// Setup nextToken without input buffer.
+func SetUnbefferedInput(r io.Reader) {
+	buf := bufio.NewReader(r)
+	var b []byte
+
+	var i int
+	nextToken = func() ([]byte, error) {
+		var err error
+		if i == len(b) {
+			b, err = buf.ReadBytes('\n')
+			if err != nil {
+				return nil, err
+			}
+			i = 0
+			j := len(b) - 1
+			for 0 <= j && IsSpace(b[j]) {
+				j--
+			}
+			b = b[0 : j+1]
+		}
+		for i < len(b) && IsSpace(b[i]) {
+			i++
+		}
+		j := i
+		for i < len(b) && !IsSpace(b[i]) {
+			i++
+		}
+		if i == j {
+			return nil, io.ErrUnexpectedEOF
+		}
+		return b[j:i], nil
+	}
+}
+
+// Set IO functions for interactive input/output.
+func SetInteractive(w io.Writer, r io.Reader) {
+	SetUnbefferedInput(r)
+	OutputBuffer = nil
+	OutputWriter = w
+}
+
+// Reads next token and return it as string
+func reads() string {
+	return string(readb())
+}
+
+// Reads next token and return it as int64
+func readll() int64 {
+	i, err := strconv.ParseInt(reads(), 10, 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return i
+}
+
+// Reads next token and return it as int
+func readi() int {
+	return int(readll())
+}
+
+// Reads next token and return it as float64
+func readf() float64 {
+	f, err := strconv.ParseFloat(reads(), 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return f
+}
+
+// set up IO functions
+func init() {
+	SetInput(os.Stdin)
+
+	// Enable below when interactive.  Its ok to leave above intact.
+	SetInteractive(os.Stdout, os.Stdin)
+}
+
+func main() {
+	N := readi()
+	inputs := make([]int, N)
+	for i := range inputs {
+		inputs[i] = readi()
+	}
+
+	alice := 0
+	bob := 0
+
+	sort.Slice(inputs, func(i, j int) bool { return inputs[i] > inputs[j] })
+
+	for i := 0; i < len(inputs); i++ {
+		if (i % 2 == 0) {
+			alice = alice + inputs[i]
+		} else {
+			bob = bob + inputs[i]
+		}
+	}
+
+	fmt.Println(alice - bob)
+}
